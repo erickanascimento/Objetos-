e2d84e5b07c9a858d163a2feeab282eaf27005f0
Ericka Nascimento <ericka.nascimento12@gmail.com>
2024-03-19 20:55:06 -0300

    Inicializei um novo repositorio

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let pessoa = {
+    Nome: 'Ericka',
+    Idade: 37,
+    Altura: 156,
+    Profissao: 'Frontend Developer',
+    Endereço: 'Rua da alegria, 23',
+    Peso: 80,
+    Telefone: '970475780',
+    Nacionalidade: 'Brasileira',
+    Estado: 'SP',
+    Cidade: 'Osasco',
+
+}
+
+
+
