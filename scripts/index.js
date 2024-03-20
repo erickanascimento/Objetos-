5368659b7419fdf8fae6ba1979d255092184eb99
Ericka Nascimento <ericka.nascimento12@gmail.com>
2024-03-20 18:52:42 -0300

    inicializei um novo repositório

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,27 @@
-let pessoa = {
-    Nome: 'Ericka',
-    Idade: 37,
-    Altura: 156,
-    Profissao: 'Frontend Developer',
-    Endereço: 'Rua da alegria, 23',
-    Peso: 80,
-    Telefone: '970475780',
-    Nacionalidade: 'Brasileira',
-    Estado: 'SP',
-    Cidade: 'Osasco',
+class Pessoa {
+    constructor (nome, sobrenome, idade, cor, cabelo, olhos, estadocivil)
+     {
+        this.nome = "Ericka",
+        this.sobrenome = "Nascimento",
+        this.idade = 37
+        this.cor = "Preta",
+        this.cabelo = "Black Power",
+        this.olhos = "Castanho escuro",
+        this.estadocivil = "Casada"
+     }  
 
-}
+    
+    apresentar = () => {
+        console.log('Olá, meu nome é ${this.nome} ${this.sobrenome}. Eu tenho ${this.idade} anos. Minha cor é ${this.cor}')
+        console.log('Meu cabelo é ${this.cabelo}. A cor dos meus olhos são ${this.olhos}. Meu estado civil é ${this.estadocivil.')
+       
+    }
 
+    }
+
+    const pessoa1 = new Pessoa("Ericka", "Nascimento", 37, "Preta", "Black Power", "Castanho escuro", "Casada",)
+    
+
+    pessoa1.apresentar()
 
 
